c24557029b2c0de8e918491bb34362574b68c02e
thinktank01 <felixayot@gmail.com>
2023-09-20 11:30:39 +0300

    Modify models/review.py to inherit from Base class as well.

--- models/review.py
+++ models/review.py
@@ -8,7 +8,7 @@ from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
