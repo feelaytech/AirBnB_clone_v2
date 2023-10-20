ef21da974b723e4f444038321dd2d718832fe812
thinktank01 <felixayot@gmail.com>
2023-10-20 11:06:00 +0300

    Fix correct display item on routes && fix Jinja2 code formatting for readability

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,7 +13,7 @@ def hello_hbnb():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns HBNB on route /hbnb."""
-    return "HBNB!"
+    return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
